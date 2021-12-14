8849ae9a86816bd724c0ac65a41afaddff7f5981
Sidney Outeiro <sid@dcc.ufrj.br>
2021-12-14 18:07:40 -0300

    preparando para o review

--- src/routers/usersRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import * as usersController from '../controllers/usersController';
-
-const router = Router();
-
-router.post('', usersController.createNewUser);
-
-export default router;
